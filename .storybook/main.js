b351633a36aed5a845929a9f0626f6b7e323e45b
hretheum <eof@offline.pl>
2025-10-12 18:38:12 +0200

    Fix Storybook config: add React dependencies and update main.js configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,13 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  stories: [
+    '../stories/**/*.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)'
+  ],
   addons: [
+    '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-a11y',
-    '@storybook/addon-links',
   ],
   framework: {
     name: '@storybook/react-vite',
@@ -12,6 +15,9 @@ const config = {
   },
   docs: {
     autodocs: 'tag',
+  },
+  core: {
+    builder: '@storybook/builder-vite',
   },
 };
 
